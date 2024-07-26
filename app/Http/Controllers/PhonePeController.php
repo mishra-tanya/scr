0087bd1685a00d0d14ac08284df3b5200468b10a
Tanya Mishra <tanyakrishanmishra@gmail.com>
2024-07-26 19:40:25 +0530

    phone pay without curl

--- app/Http/Controllers/PhonePeController.php
+++ app/Http/Controllers/PhonePeController.php
@@ -3,13 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Http;
-
 use Illuminate\Support\Facades\Auth;
-
 use Illuminate\Support\Facades\DB; 
-use Ixudra\Curl\Facades\Curl;
 use App\Models\Reg_User;
 
 class PhonePeController extends Controller
@@ -18,8 +14,8 @@ class PhonePeController extends Controller
     {
         $user = Auth::user();
         $transactionId = uniqid(); 
-        $userId=$user->id;
-// dd($userId);
+        $userId = $user->id;
+
         DB::table('transactions')->insert([
             'transaction_id' => $transactionId,
             'user_id' => $userId,
@@ -27,7 +23,7 @@ class PhonePeController extends Controller
             'updated_at' => now(),
         ]);
 
-        $data = array (
+        $data = [
             'merchantId' => env('PHONEPE_MERCHANT_ID'),
             'merchantTransactionId' => $transactionId,
             'merchantUserId' => 'MUID123',
@@ -35,33 +31,30 @@ class PhonePeController extends Controller
             'redirectUrl' => route('phonepe.callback'),
             'redirectMode' => 'POST',
             'callbackUrl' => route('phonepe.callback'),
-            // 'mobileNumber' => '9999999999',
-            'paymentInstrument' => 
-            array (
-            'type' => 'PAY_PAGE',
-            ),
-        );
+            'paymentInstrument' => [
+                'type' => 'PAY_PAGE',
+            ],
+        ];
         $encode = base64_encode(json_encode($data));
 
         $saltKey = env('PHONEPE_API_KEY');
         $saltIndex = 1;
 
-        $string = $encode.'/pg/v1/pay'.$saltKey;
-        $sha256 = hash('sha256',$string);
+        $string = $encode . '/pg/v1/pay' . $saltKey;
+        $sha256 = hash('sha256', $string);
 
-        $finalXHeader = $sha256.'###'.$saltIndex;
+        $finalXHeader = $sha256 . '###' . $saltIndex;
 
         $url = "https://api.phonepe.com/apis/hermes/pg/v1/pay";
-        $response = Curl::to($url)
-                ->withHeader('Content-Type:application/json')
-                ->withHeader('X-VERIFY:'.$finalXHeader)
-                ->withData(json_encode(['request' => $encode]))
-                ->post();
+        $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'X-VERIFY' => $finalXHeader
+            ])
+            ->post($url, ['request' => $encode]);
+        $rData = $response->json();
+        // dd($rData);
 
-        // dd($response);
-        $rData = json_decode($response);
-
-        return redirect()->to($rData->data->instrumentResponse->redirectInfo->url);
+        return redirect()->to($rData['data']['instrumentResponse']['redirectInfo']['url']);
     }
 
     public function handleCallback(Request $request)
@@ -70,37 +63,37 @@ class PhonePeController extends Controller
         $saltKey = env('PHONEPE_API_KEY');
         $saltIndex = 1;
 
-        $finalXHeader = hash('sha256','/pg/v1/status/'.$input['merchantId'].'/'.$input['transactionId'].$saltKey).'###'.$saltIndex;
+        $finalXHeader = hash('sha256', '/pg/v1/status/' . $input['merchantId'] . '/' . $input['transactionId'] . $saltKey) . '###' . $saltIndex;
 
-        $response = Curl::to('https://api.phonepe.com/apis/hermes/pg/v1/pay/status/'.$input['merchantId'].'/'.$input['transactionId'])
-                ->withHeader('Content-Type:application/json')
-                ->withHeader('accept:application/json')
-                ->withHeader('X-VERIFY:'.$finalXHeader)
-                ->withHeader('X-MERCHANT-ID:'.$input['transactionId'])
-                ->get();
+        $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'accept' => 'application/json',
+                'X-VERIFY' => $finalXHeader,
+                'X-MERCHANT-ID' => $input['transactionId']
+            ])
+            ->get('https://api.phonepe.com/apis/hermes/pg/v1/status/' . $input['merchantId'] . '/' . $input['transactionId']);
 
-                $responseData = json_decode($response);
-                // dd($responseData);
-
-                if ($responseData->data->responseCode == 'SUCCESS') {
-                    $transaction = DB::table('transactions')
-                    ->where('transaction_id', $input['transactionId'])
-                    ->first();
-        
-                if ($transaction) {
-                    $user = Reg_User::find($transaction->user_id);
-                    
-                    if ($user) {
-                        $user->payment_status = 1;
-                        $user->payment_id = $input['transactionId'];
-                        $user->save();
-                    }
-        
-                    // DB::table('transactions')->where('transaction_id', $input['transactionId'])->delete();
+        $responseData = $response->json();
+        // dd($responseData);
+        if ($responseData['data']['responseCode'] == 'SUCCESS') {
+            $transaction = DB::table('transactions')
+                ->where('transaction_id', $input['transactionId'])
+                ->first();
+    
+            if ($transaction) {
+                $user = Reg_User::find($transaction->user_id);
+                
+                if ($user) {
+                    $user->payment_status = 1;
+                    $user->payment_id = $input['transactionId'];
+                    $user->save();
                 }
-                return redirect()->route('payment.success')->with('status', 'Payment successful!');
+    
+                // DB::table('transactions')->where('transaction_id', $input['transactionId'])->delete();
+            }
+            return redirect()->route('payment.success')->with('status', 'Payment successful!');
+        }
 
-                }
         return redirect()->route('payment.failed')->with('status', 'Payment failed!');
     }
 }
